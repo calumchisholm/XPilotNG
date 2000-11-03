4f3b463568922a996100d6d4a93c608224387c4c
uau <>
2000-11-03 13:03:20 +0000

    Minor fix.

--- xpilot/mapconvert.py
+++ xpilot/mapconvert.py
@@ -318,8 +318,8 @@ def convert(options):
     for p in polys2:
 	for l in p:
 	    pass
-#	    l[0] = int(l[0] + sin(1. * l[0] * l[1] / 2786) * (2240 / 3)) % mxc
-#	    l[1] = int(l[1] + sin(1. * l[0] * l[1] / 1523) * (2240 / 3)) % myc
+	    l[0] = int(l[0] + sin(1. * l[0] * l[1] / 2786) * (2240 / 3)) % mxc
+	    l[1] = int(l[1] + sin(1. * l[0] * l[1] / 1523) * (2240 / 3)) % myc
 
     print "<XPilotMap>"
 
@@ -353,12 +353,12 @@ def convert(options):
 	print '<BallArea team="%d">' % ball.team
 	print '<Polygon x="%d" y="%d">' % (ball.x - 1120, ball.y - 640)
 	print '<Offset x="2240" y="0"/> <Offset x="0" y="2240"/>'
-	print '<Offset x="-2240" y="0"/> <Offset x="0" y="-2240"/>'
+	print '<Offset x="-2240" y="0"/>'
 	print '</Polygon></BallArea>'
 	print '<BallTarget team="%d">' % ball.team
 	print '<Polygon x="%d" y="%d">' % (ball.x - 480, ball.y)
 	print '<Offset x="960" y="0"/> <Offset x="0" y="960"/>'
-	print '<Offset x="-960" y="0"/> <Offset x="0" y="-960"/>'
+	print '<Offset x="-960" y="0"/>'
 	print '</Polygon></BallTarget>'
     for base in bases:
 	print '<Base team="%d" x="%d" y="%d" dir = "%d"/>' % \
