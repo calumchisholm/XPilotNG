446f17c634d7a96e1cf0361e2c5a3ee438e2ec2d
kps <>
2003-06-17 14:25:38 +0000

    Removed <config.h> include for now.

--- xpilot/src/server/shipshape_s.c
+++ xpilot/src/server/shipshape_s.c
@@ -21,10 +21,6 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#if HAVE_CONFIG_H
-#  include <config.h>
-#endif
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
