7fafce2c7953e2468754e1cfff6dbd86cd537b32
kps <>
2003-06-18 21:47:26 +0000

    Added more checks for headers in configure.ac. Changed xpcommon.h to conditionally include only found headers.

--- xpilot/src/common/xpcommon.h
+++ xpilot/src/common/xpcommon.h
@@ -24,72 +24,185 @@
 #ifndef XPCOMMON_H
 #define XPCOMMON_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <math.h>
-#include <limits.h>
-#include <signal.h>
-#include <setjmp.h>
-#include <string.h>
-#include <time.h>
-
-#include <sys/types.h>
-#include <sys/file.h>
-#include <sys/stat.h>
-
-#ifndef _WINDOWS
-# include <unistd.h>
-# include <pwd.h>
-# ifndef __hpux
-#  include <sys/time.h>
-# endif
-# ifdef _AIX
-#  ifndef _BSD_INCLUDES
-#   define _BSD_INCLUDES
-#  endif
-#  include <sys/select.h>
-# endif
-# include <sys/file.h>
-# include <sys/param.h>
-# include <sys/socket.h>
-# include <sys/ioctl.h>
-# include <netinet/in.h>
-# include <netinet/tcp.h>
-# include <arpa/inet.h>
-# include <netdb.h>
-# if defined(SVR4) || defined(__svr4__)
-#  include <sys/sockio.h>
-# endif
-# include <net/if.h>
-#endif
-
-#ifdef __sgi
-# include <bstring.h>
-#endif
-
-#ifdef SVR4
-# include <sys/filio.h>
-#endif
-
-#ifdef _SEQUENT_
-# include <sys/fcntl.h>
+#ifdef HAVE_CONFIG_H
+# include <config.h>
 #else
+/* If we are not using autoconf, we do the best we can. */
+# define STDC_HEADERS 1
+# define HAVE_LIMITS_H 1
+# define HAVE_SETJMP_H 1
+# define HAVE_STDLIB_H 1
+# define HAVE_STRING_H 1
+# define HAVE_SYS_FILE_H 1
+# define HAVE_SYS_STAT_H 1
+# define HAVE_SYS_TYPES_H 1
+# ifndef _WINDOWS
+#  define HAVE_PWD_H 1
+#  define HAVE_UNISTD_H 1
+#  define HAVE_ARPA_INET_H 1
+#  define HAVE_NETINET_IN_H 1
+#  define HAVE_NETINET_TCP_H 1
+#  define HAVE_NET_IF_H 1
+#  define HAVE_NETDB_H 1
+#  define HAVE_SYS_IOCTL_H 1
+#  define HAVE_SYS_PARAM_H 1
+#  define HAVE_SYS_SELECT_H 1
+#  define HAVE_SYS_SOCKET_H 1
+#  ifdef _AIX
+#   ifndef _BSD_INCLUDES
+#    define _BSD_INCLUDES 1
+#   endif
+#  endif
+#  ifndef __hpux
+#   define HAVE_SYS_TIME_H 1
+#   define TIME_WITH_SYS_TIME 1
+#  endif
+#  ifndef _SEQUENT_
+#   define HAVE_FCNTL_H 1
+#  else
+#   define HAVE_SYS_FCNTL_H 1
+#  endif
+#  ifdef __sgi
+#   define HAVE_BSTRING_H 1
+#  endif
+#  ifdef __sun__
+#   define HAVE_ARPA_NAMESER_H 1
+#   define HAVE_RESOLV_H 1
+#  endif
+#  if defined(SVR4) || defined(__svr4__)
+#   define HAVE_SYS_FILIO_H 1
+#   define HAVE_SYS_SOCKIO_H 1
+#  endif
+# endif /* _WINDOWS */
+#endif
+
+#ifdef STDC_HEADERS
+# include <stdio.h>
+# include <assert.h>
+# include <ctype.h>
+# include <errno.h>
+# include <math.h>
+# include <signal.h>
+# include <time.h>
+#endif
+
+#ifdef HAVE_SETJMP_H
+# include <setjmp.h>
+#endif
+
+#ifdef HAVE_STDLIB_H
+# include <stdlib.h>
+#endif
+
+#ifdef HAVE_LIMITS_H
+# include <limits.h>
+#endif
+
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
+
+#ifdef HAVE_FCNTL_H
 # include <fcntl.h>
 #endif
 
-#ifdef __sun__
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+#ifdef HAVE_PWD_H
+# include <pwd.h>
+#endif
+
+#ifdef HAVE_SYS_FILE_H
+# include <sys/file.h>
+#endif
+
+#ifdef HAVE_SYS_IOCTL_H
+# include <sys/ioctl.h>
+#endif
+
+#ifdef HAVE_SYS_PARAM_H
+# include <sys/param.h>
+#endif
+
+#ifdef HAVE_SYS_SELECT_H
+# include <sys/select.h>
+#endif
+
+#ifdef HAVE_SYS_SOCKET_H
+# include <sys/socket.h>
+#endif
+
+#ifdef HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+
+#if defined(HAVE_SYS_TIME_H) && defined(TIME_WITH_SYS_TIME)
+# include <sys/time.h>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+
+#ifdef HAVE_ARPA_INET_H
+# include <arpa/inet.h>
+#endif
+
+#ifdef HAVE_NET_IF_H
+# include <net/if.h>
+#endif
+
+#ifdef HAVE_NETINET_IN_H
+# include <netinet/in.h>
+#endif
+
+#ifdef HAVE_NETINET_TCP_H
+# include <netinet/tcp.h>
+#endif
+
+#ifdef HAVE_NETDB_H
+# include <netdb.h>
+#endif
+
+/* Platform specific hacks. */
+
+/* SGI hack. */
+#ifdef HAVE_BSTRING_H
+# include <bstring.h>
+#endif
+
+/* System V R4 hacks. */
+#ifdef HAVE_SYS_FILIO_H
+# include <sys/filio.h>
+#endif
+
+#ifdef HAVE_SYS_SOCKIO_H
+# include <sys/sockio.h>
+#endif
+
+/* Sequent hack. */
+#ifdef HAVE_SYS_FCNTL_H
+# include <sys/fcntl.h>
+#endif
+
+/* Sun hacks. */
+#ifdef HAVE_ARPA_NAMESER_H
 # include <arpa/nameser.h>
+#endif
+
+#ifdef HAVE_RESOLV_H
 # include <resolv.h>
 #endif
 
+/* VMS hacks. */
 #ifdef VMS
 # include <socket.h>
 # include <in.h>
 #endif
 
+/* Evil Windows hacks. Yuck. */
 #if defined(_WINDOWS)
 # include "NT/winNet.h"
   /* need this for printf wrappers. */
@@ -118,6 +231,8 @@ static void Win_show_error(char *errmsg);
 # undef write
 # define write(x__, y__, z__) send(x__, y__, z__,0)
 #endif
+
+/* Common XPilot header files. */
 
 #include "version.h"
 #include "xpconfig.h"
