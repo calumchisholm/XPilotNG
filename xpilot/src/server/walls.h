353e1bb916256b1a39a1dd60251a3271fd0d6b21
kps <>
2003-09-09 20:45:18 +0000

    removed move_bounce_t.

--- xpilot/src/server/walls.h
+++ xpilot/src/server/walls.h
@@ -29,8 +29,6 @@
 #include "click.h"
 #endif
 
-extern char walls_version[];
-
 /*
  * Wall collision detection and bouncing.
  *
@@ -60,19 +58,6 @@ typedef enum {
     CrashWallAngle = 0x200
 } move_crash_t;
 
-typedef enum {
-    NotABounce = 0,
-    BounceLeft = 0x01,
-    BounceRight = 0x02,
-    BounceDown = 0x04,
-    BounceUp = 0x08,
-    BounceLeftDown = 0x10,
-    BounceLeftUp = 0x20,
-    BounceRightDown = 0x40,
-    BounceRightUp = 0x80,
-    BounceEdge = 0x0100
-} move_bounce_t;
-
 typedef struct {
     int			edge_wrap;
     int			edge_bounce;
@@ -89,7 +74,6 @@ typedef struct {
 typedef struct {
     const move_info_t	*mip;
     move_crash_t	crash;
-    move_bounce_t	bounce;
     clpos		pos;
     vector		vel;
     clvec		todo;
