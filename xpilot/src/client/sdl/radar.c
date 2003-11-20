c8d957c77e3a4357e4fb7f4fb50532e9c297834f
uau <>
2003-11-20 17:52:30 +0000

    Fixed 2 bugs that confused the x and y dimensions, and thus broke the radar on maps with unequal width and height.

--- xpilot/src/client/sdl/radar.c
+++ xpilot/src/client/sdl/radar.c
@@ -22,31 +22,30 @@ static GLuint      radar_texture;     /* above as an OpenGL texture */
      | ((RGB) & 0x00ff00) \
      | ((RGB) & 0x0000ff) << 16)
 
-static int pow2_ceil(int t) 
+static int pow2_ceil(int t)
 {
     int r;
     for (r = 1; r < t; r <<= 1);
-    return r;   
+    return r;
 }
 
 static void Radar_paint_border(void)
 {
     glBegin(GL_LINE_LOOP);
     glColor4ub(0, 0, 0, 0xff);
-    glVertex2i(radar_bounds.x, radar_bounds.y + radar_bounds.h);    
+    glVertex2i(radar_bounds.x, radar_bounds.y + radar_bounds.h);
     glColor4ub(0, 0x00, 0x90, 0xff);
     glVertex2i(radar_bounds.x, radar_bounds.y);
     glColor4ub(0, 0, 0, 0xff);
     glVertex2i(radar_bounds.x + radar_bounds.w, radar_bounds.y);
     glColor4ub(0, 0x00, 0x90, 0xff);
-    glVertex2i(radar_bounds.x + radar_bounds.w, 
+    glVertex2i(radar_bounds.x + radar_bounds.w,
 	       radar_bounds.y + radar_bounds.h);
     glEnd();
-    
 }
 
 /*
- * Paints a block in the radar to the given position using the 
+ * Paints a block in the radar to the given position using the
  * given color. This one doesn't do any locking on the surface.
  */
 static void Radar_paint_block(SDL_Surface *s, int xi, int yi, color_t color)
@@ -82,12 +81,10 @@ static void Radar_paint_world_blocks(SDL_Surface *s)
 	bcolor[SETUP_REC_LD] =
 	bcolor[SETUP_REC_RD] =
 	bcolor[SETUP_FUEL] = wallRadarColorValue;
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++)
 	bcolor[SETUP_TARGET+i] = targetRadarColorValue;
-    }
-    for (i = BLUE_BIT; i < 256; i++) {
+    for (i = BLUE_BIT; i < 256; i++)
 	bcolor[i] = wallRadarColorValue;
-    }
     if (instruments.showDecor) {
 	bcolor[SETUP_DECOR_FILLED] =
 	    bcolor[SETUP_DECOR_LU] =
@@ -109,9 +106,9 @@ static void Radar_paint_world_blocks(SDL_Surface *s)
                 && type < SETUP_TARGET + 10
                 && !Target_alive(xi, yi, &damage))
                 type = SETUP_SPACE;
-            
+
             color = bcolor[type];
-            if (color & 0xffffff) 
+            if (color & 0xffffff)
                 Radar_paint_block(s, xi, yi, color);
         }
     }
@@ -142,10 +139,10 @@ static void Radar_paint_world_polygons(SDL_Surface *s)
     ipos min, max;
     Sint16 vx[MAX_VERTICES], vy[MAX_VERTICES];
     color_t color;
-    
+
     if (SDL_MUSTLOCK(s)) SDL_LockSurface(s);
     SDL_FillRect(s, NULL, RGBA(bgRadarColorValue));
-    
+
     for (i = 0; i < num_polygons; i++) {
 
 	if (BIT(polygon_styles[polygons[i].style].flags,
@@ -153,22 +150,22 @@ static void Radar_paint_world_polygons(SDL_Surface *s)
 	Compute_bounds(&min, &max, &polygons[i].bounds);
 
 	for (xoff = min.x; xoff <= max.x; xoff++) {
-	    for (yoff = min.y; yoff <= max.y; yoff++) {		
+	    for (yoff = min.y; yoff <= max.y; yoff++) {
 
 		int x = polygons[i].points[0].x + xoff * Setup->width;
 		int y = -polygons[i].points[0].y + (1-yoff) * Setup->height;
 		vx[0] = (x * radar_bounds.w) / Setup->width;
 		vy[0] = (y * radar_bounds.h) / Setup->height;
-		
-		for (j = 1; j < polygons[i].num_points; j++) {		    
+
+		for (j = 1; j < polygons[i].num_points; j++) {
 		    x += polygons[i].points[j].x;
 		    y -= polygons[i].points[j].y;
 		    vx[j]= (x * radar_bounds.w) / Setup->width;
 		    vy[j] = (y * radar_bounds.h) / Setup->height;
 		}
-		
+
 		color = polygon_styles[polygons[i].style].rgb;
-		filledPolygonRGBA(s, vx, vy, j, 
+		filledPolygonRGBA(s, vx, vy, j,
 				  (color >> 16) & 0xff,
 				  (color >> 8) & 0xff,
 				  color & 0xff,
@@ -193,7 +190,7 @@ static void Radar_paint_objects(void)
 	sx = selfPos.x * rb.w / Setup->width;
 	sy = selfPos.y * rb.h / Setup->height;
     }
-    
+
     for (i = 0; i < num_radar; i++) {
         fx = radar_ptr[i].x * rb.w / RadarWidth;
         fy = radar_ptr[i].y * rb.h / RadarHeight;
@@ -211,7 +208,7 @@ static void Radar_paint_objects(void)
 	    else if (fy > rb.h/2)
 		fy -= rb.h;
 	    fx += rb.w/2;
-	    fy += rb.w/2;
+	    fy += rb.h/2;
         }
 
 	x = rb.x + (fx + 0.5) - s/2;
@@ -229,10 +226,9 @@ static void Radar_paint_objects(void)
     }
 
     glEnd();
-    
-    if (num_radar) {
+
+    if (num_radar)
 	RELEASE(radar_ptr, num_radar, max_radar);
-    }
 }
 
 /*
@@ -252,7 +248,7 @@ static void Radar_paint_checkpoints(float xf, float yf)
 /*
  * The radar is drawn so that first the walls are painted to an offscreen
  * SDL surface. This surface is then converted into an OpenGL texture.
- * For each frame OpenGL is used to paint rectangles with this walls 
+ * For each frame OpenGL is used to paint rectangles with this walls
  * texture and on top of that the radar objects.
  */
 int Radar_init(void)
@@ -260,11 +256,11 @@ int Radar_init(void)
     radar_bounds.x = 10;
     radar_bounds.y = 10;
     radar_bounds.w = 200;
-    radar_bounds.h = 200 * RadarWidth / RadarHeight;
-    
+    radar_bounds.h = 200 * RadarHeight / RadarWidth;
+
     radar_surface =
 	SDL_CreateRGBSurface(SDL_SWSURFACE | SDL_SRCALPHA,
-                             pow2_ceil(radar_bounds.w), 
+                             pow2_ceil(radar_bounds.w),
 			     pow2_ceil(radar_bounds.h), 32,
                              RMASK, GMASK, BMASK, AMASK);
     if (!radar_surface) {
@@ -277,13 +273,13 @@ int Radar_init(void)
 
     glGenTextures(1, &radar_texture);
     glBindTexture(GL_TEXTURE_2D, radar_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
 		 radar_surface->w, radar_surface->h,
-                 0, GL_RGBA, GL_UNSIGNED_BYTE, 
+                 0, GL_RGBA, GL_UNSIGNED_BYTE,
 		 radar_surface->pixels);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, 
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
                     GL_NEAREST);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, 
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                     GL_NEAREST);
     return 0;
 }
@@ -333,49 +329,46 @@ void Radar_paint(void)
         int x, y, w, h;
         float xp, yp, xo, yo;
         SDL_Rect sr, dr;
-        
+
         xp = (float) (selfPos.x * radar_bounds.w) / Setup->width;
         yp = (float) (selfPos.y * radar_bounds.h) / Setup->height;
         xo = (float) radar_bounds.w / 2;
         yo = (float) radar_bounds.h / 2;
-        if (xo <= xp) {
+        if (xo <= xp)
             x = (int) (xp - xo + 0.5);
-        } else {
+	else
             x = (int) (radar_bounds.w + xp - xo + 0.5);
-        }
-        if (yo <= yp) {
+        if (yo <= yp)
             y = (int) (yp - yo + 0.5);
-        } else {
+	else
             y = (int) (radar_bounds.h + yp - yo + 0.5);
-        }
 	/* CB fixed radar bug   y = radar_bounds.h - y - 1; */
 	y = radar_bounds.h - y;
         w = radar_bounds.w - x;
         h = radar_bounds.h - y;
-        
+
         sr.x = 0; sr.y = 0; sr.w = x; sr.h = y;
-        dr.x = w + radar_bounds.x; dr.y = h + radar_bounds.y; 
+        dr.x = w + radar_bounds.x; dr.y = h + radar_bounds.y;
         dr.w = x; dr.h = y;
         Radar_blit_world(&sr, &dr);
-        
+
         sr.x = x; sr.y = 0; sr.w = w; sr.h = y;
-        dr.x = 0 + radar_bounds.x; dr.y = h + radar_bounds.y; 
+        dr.x = 0 + radar_bounds.x; dr.y = h + radar_bounds.y;
         dr.w = w; dr.h = y;
         Radar_blit_world(&sr, &dr);
-        
+
         sr.x = 0; sr.y = y; sr.w = x; sr.h = h;
-        dr.x = w + radar_bounds.x; dr.y = 0 + radar_bounds.y; 
+        dr.x = w + radar_bounds.x; dr.y = 0 + radar_bounds.y;
         dr.w = x; dr.h = h;
         Radar_blit_world(&sr, &dr);
-        
+
         sr.x = x; sr.y = y; sr.w = w; sr.h = h;
-        dr.x = 0 + radar_bounds.x; dr.y = 0 + radar_bounds.y; 
+        dr.x = 0 + radar_bounds.x; dr.y = 0 + radar_bounds.y;
         dr.w = w; dr.h = h;
         Radar_blit_world(&sr, &dr);
-
     } else {
 	SDL_Rect sr;
-	sr.x = sr.y = 0; 
+	sr.x = sr.y = 0;
 	sr.w = radar_bounds.w; sr.h = radar_bounds.h;
 	Radar_blit_world(&sr, &radar_bounds);
     }
