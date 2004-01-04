7496ba0894a6c4e26a66142a4869c8f3bcb15546
kps <>
2004-01-04 13:39:51 +0000

    Removed some unused parameter warnings.

--- xpilot/src/client/widget.c
+++ xpilot/src/client/widget.c
@@ -906,6 +906,7 @@ static Bool Widget_check_motion(Display *d, XEvent *e, char *p)
 {
     struct widget_check_event	*cm = (struct widget_check_event *) p;
 
+    (void)d;
     if (e->xany.window == cm->window) {
 	if (e->type == MotionNotify)
 	    cm->found++;
@@ -1210,6 +1211,7 @@ static void Widget_inside(XEvent *event, int widget_desc, bool inside)
     widget_activate_t	*activw;
     widget_arrow_t	*arroww;
 
+    (void)event;
     if ((widget = Widget_pointer(widget_desc)) == NULL) {
 	warn("Widget inside invalid");
 	return;
@@ -2183,6 +2185,7 @@ static int Widget_viewer_save_callback(int widget_desc, void *data,
     widget_viewer_t	*viewer_sub;
     FILE		*fp;
 
+    (void)widget_desc; (void)strptr;
     formw = (widget_form_t *)popup->sub;
     viewer_desc = formw->children[0];
     viewer_widget = Widget_pointer(viewer_desc);
@@ -2198,6 +2201,7 @@ static int Widget_viewer_save_callback(int widget_desc, void *data,
 static int Widget_viewer_close_callback(int widget_desc, void *data,
 					const char **strptr)
 {
+    (void)widget_desc; (void)strptr;
     Widget_unmap((int)(long)data);
     return 0;
 }
