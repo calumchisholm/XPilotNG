41ccc2f68c397fbd44a70133a3decc93ea92b42a
darelcullen <>
2004-08-30 18:55:50 +0000

    fix the path now that bitmaps has moved.. guess bitmaps should really be copied into NT instead, this is a fix just to flag this fault for now.

--- xpilot/src/client/NT/winBitmap.h
+++ xpilot/src/client/NT/winBitmap.h
@@ -24,7 +24,7 @@
 #ifndef WINBITMAP_H
 #define WINBITMAP_H
 #include "../gfx2d.h"
-#include "../bitmaps.h"
+#include "../x11/bitmaps.h"
 extern void PaintBitmap(Drawable d, int type, int x, int y,
 			int width, int height, int number);
 
