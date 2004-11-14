47f1963b184889ff3d97e16fea792c7e03ad35c4
kps <>
2004-11-14 14:35:14 +0000

    Added example configuring script with a lot of warnings (for developers).

---
+++ xpilot/kps_configure.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+
+export CC=gcc
+#export CC=g++
+
+# don't use these with g++
+export CFLAGS_C=" \
+-pedantic \
+-Wmissing-declarations \
+-Wnested-externs \
+-Wsequence-point \
+-Wbad-function-cast \
+"
+
+export CFLAGS=" \
+-O2 \
+-g \
+-Wall \
+-Wmissing-prototypes \
+-Wstrict-prototypes \
+-Wredundant-decls \
+-Wshadow \
+-Wswitch \
+-Wparentheses \
+-Wendif-labels \
+-Wundef \
+-Wmissing-braces \
+-Wcomment \
+-Wunused \
+-Wreturn-type \
+-Wpointer-arith \
+-Wsign-compare \
+-Wcast-align \
+-Wcast-qual \
+-Wuninitialized \
+-Wimplicit \
+-Winline \
+-Wchar-subscripts \
+-Wtrigraphs \
+-Wformat=2 \
+-Wwrite-strings \
+-Wimport \
+-Wdisabled-optimization \
+"
+
+if [ "x$CC" != "xg++" ]; then
+    CFLAGS="$CFLAGS $CFLAGS_C"
+fi
+
+./configure --prefix=$HOME --enable-sdl-client
