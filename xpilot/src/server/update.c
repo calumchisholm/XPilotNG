d3f70da7f2b47c856aa4853665b2a79eaae910cb
iridos <>
2005-01-18 10:19:09 +0000

    added kps's Player_add_score change to survival scoring; updateScores is set twice?!

--- xpilot/src/server/update.c
+++ xpilot/src/server/update.c
@@ -842,7 +842,7 @@ static void Update_players(world_t *world)
         if (Player_is_alive(pl)
 	    && !BIT(pl->used, HAS_SHIELD)) {
 	    if (options.survivalScore != 0.0) {
-		pl->score += pl->survival_time*options.survivalScore/FPS;
+	        Player_add_score(pl, pl->survival_time*options.survivalScore/FPS);
 		updateScores = true;
 	    }
 	    pl->survival_time+= timePerFrame;
