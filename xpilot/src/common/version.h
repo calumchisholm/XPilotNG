d34bf386eb5fc5ee70fde7e4c7871b11b63fde98
kps <>
2005-04-10 17:09:35 +0000

    version is 4.7.1 soon

--- xpilot/src/common/version.h
+++ xpilot/src/common/version.h
@@ -30,9 +30,9 @@
 #ifndef	VERSION_H
 #define	VERSION_H
 
-#define TITLE			"XPilot NG 4.7.0"
+#define TITLE			"XPilot NG 4.7.1"
 #ifndef VERSION
-#  define VERSION		"4.7.0"
+#  define VERSION		"4.7.1"
 #endif
 #define AUTHORS			"Bjørn Stabell, Ken Ronny Schouten, Bert Gijsbers, Dick Balaska, Uoti Urpala, Juha Lindström, Kristian Söderblom and Erik Andersson"
 #define COPYRIGHT		"Copyright © 1991-2005 by Bjørn Stabell, Ken Ronny Schouten, Bert Gijsbers, Dick Balaska, Uoti Urpala, Juha Lindström, Kristian Söderblom and Erik Andersson"
